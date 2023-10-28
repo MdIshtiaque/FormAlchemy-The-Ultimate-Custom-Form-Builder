7dc0bd9ff2ea123bb89655e079c3b7e6de539862
Md Ishtiaque Ferdous <88388417+MdIshtiaque@users.noreply.github.com>
2023-10-28 14:45:42 +0600

    Added Register Button

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -43,5 +43,13 @@
                 {{ __('Log in') }}
             </x-primary-button>
         </div>
+        <div class="flex items-center justify-end mt-4">
+            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">
+                {{ __('Do not have account?') }}
+            </a>
+            <a href="{{ route('register') }}" class="ml-3 inline-flex items-center px-4 py-2 bg-indigo-600 border rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-indigo-700 active:bg-indigo-900 focus:outline-none focus:border-indigo-900 focus:ring focus:ring-indigo-200 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
+                {{ __('Register Now') }}
+            </a>
+        </div>
     </form>
 </x-guest-layout>
