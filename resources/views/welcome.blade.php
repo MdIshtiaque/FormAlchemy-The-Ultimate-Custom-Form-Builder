e49e90772e7677ce5da761c8d7a5865961bc159d
Md Ishtiaque Ferdous <88388417+MdIshtiaque@users.noreply.github.com>
2023-10-28 14:24:20 +0600

    update Homepage design

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -55,23 +55,45 @@
             background-color: white;
             color: black;
         }
+        .form {
+            white-space: nowrap;
+        }
+        .alchemy {
+            white-space: nowrap;
+        }
+
+        @media only screen and (max-width: 768px) {
+            .waviy span {
+                font-size: 2rem;
+            }
+            .space {
+                display: none;  /* Hide the space on mobile */
+            }
+            /* other adjustments as necessary */
+        }
+
     </style>
 </head>
 <body>
 <div class="container text-center d-flex justify-content-center align-items-center vh-100">
     <div>
         <div class="waviy">
-            <span style="--i:1">F</span>
-            <span style="--i:2">o</span>
-            <span style="--i:3">r</span>
-            <span style="--i:4; padding-right: 50px">m</span>
-            <span style="--i:5">A</span>
-            <span style="--i:6">l</span>
-            <span style="--i:7">c</span>
-            <span style="--i:8">h</span>
-            <span style="--i:9">e</span>
-            <span style="--i:10">m</span>
-            <span style="--i:11">y</span>
+            <div class="form" style="display: inline-block; white-space: nowrap;">
+                <span style="--i:1">F</span>
+                <span style="--i:2">o</span>
+                <span style="--i:3">r</span>
+                <span style="--i:4">m</span>
+            </div>
+            <span class="space">&nbsp;&nbsp;&nbsp;</span>
+            <div class="alchemy" style="display: inline-block; white-space: nowrap;">
+                <span style="--i:5">A</span>
+                <span style="--i:6">l</span>
+                <span style="--i:7">c</span>
+                <span style="--i:8">h</span>
+                <span style="--i:9">e</span>
+                <span style="--i:10">m</span>
+                <span style="--i:11">y</span>
+            </div>
         </div>
         <br>
         <br>
